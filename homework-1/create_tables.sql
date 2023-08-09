107548eb691660111961396f05b7b987d60387e8
maria <buzinamn@yandex.ru>
2023-08-10 01:46:01 +0500

    all_work

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id varchar(100) PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id varchar(100) PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id),
+	employee_id varchar(100) REFERENCES employees(employee_id)
+);
+
+SELECT * FROM employees
+SELECT * FROM customers
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+FILE_CUSTOMERS = "/home/maria/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv"
+FILE_EMPLOYEES = "/home/maria/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv"
+FILE_ORDERS = "/home/maria/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv"
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1234')
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open(FILE_CUSTOMERS, newline='') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for row in reader:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s)', (row[0], row[1]))
+                    cur.execute('SELECT * FROM customers')
+            
+            with open(FILE_EMPLOYEES, newline='') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for row in reader:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s)', (row[0], row[1], row[2]))
+                    cur.execute('SELECT * FROM employees')
+
+            with open(FILE_ORDERS, newline='') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for row in reader:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s)', (row[0], row[1], row[2]))
+                    cur.execute('SELECT * FROM orders')
+
+finally:
+    conn.close()
