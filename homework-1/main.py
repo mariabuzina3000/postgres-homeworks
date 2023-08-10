4c7d907f8257ee71a8402901cb835fa8d493fafb
maria <buzinamn@yandex.ru>
2023-08-10 21:58:51 +0500

    FIX

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,16 +2,17 @@
 
 import psycopg2
 import csv
+from pathlib import Path
 
-FILE_CUSTOMERS = "/home/maria/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv"
-FILE_EMPLOYEES = "/home/maria/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv"
-FILE_ORDERS = "/home/maria/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv"
+PATH_CUSTOMERS = Path("north_data", "customers_data.csv")
+PATH_EMPLOYEES = Path("north_data", "employees_data.csv")
+PATH_ORDERS = Path("north_data", "orders_data.csv")
 
 conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='1234')
 try:
     with conn:
         with conn.cursor() as cur:
-            with open(FILE_CUSTOMERS, newline='') as csvfile:
+            with open(PATH_CUSTOMERS, newline='') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
                 # Пропускаем первую строку
                 next(reader)
@@ -19,7 +20,7 @@ try:
                     cur.execute('INSERT INTO customers VALUES (%s, %s)', (row[0], row[1]))
                     cur.execute('SELECT * FROM customers')
             
-            with open(FILE_EMPLOYEES, newline='') as csvfile:
+            with open(PATH_EMPLOYEES, newline='') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
                 # Пропускаем первую строку
                 next(reader)
@@ -27,7 +28,7 @@ try:
                     cur.execute('INSERT INTO employees VALUES (%s, %s, %s)', (row[0], row[1], row[2]))
                     cur.execute('SELECT * FROM employees')
 
-            with open(FILE_ORDERS, newline='') as csvfile:
+            with open(PATH_ORDERS, newline='') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
                 # Пропускаем первую строку
                 next(reader)
